dfe43e752323d0f81d1b9bd42ce46d70c379717e
jitender <jitender.yadav.dublin@gmail.com>
2017-09-14 11:21:49 +0100

    rest framework barebone added

---
+++ server/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from api.serializers import UserSerializer
+
+# Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allowrs users to be viewed or edited
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
